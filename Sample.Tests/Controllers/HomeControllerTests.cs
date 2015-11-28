2a70f98897bb04ba0d26f2a369941cc85e23328e
Rick Mather <northsideuk@gmail.com>
2015-11-28 09:22:04 +0000

    Add test project.

---
+++ Sample.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Sample.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sample.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Home_controller_returns_view()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ActionResult>());
+        }
+    }
+}
